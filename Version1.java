e214296612b7a5d176f1457ea063e6d0023a7ef2
unknown <sellesehlstedt@gmail.com>
2019-03-18 13:41:15 +0100

    Initial commit

---
+++ Version1.java
@@ -0,0 +1,17 @@
+package Hiyo;
+
+/*
+ * Uppgift 8.3
+ */
+public class Version1 {
+
+	//returnerar true om number är primtal, annars false
+	public static boolean ärEttPrimtal(double number) {
+		return false;
+	}
+
+	public static void main(String[] arg) {
+		
+	}
+
+}
